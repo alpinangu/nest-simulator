16106d0127d0708151fe12b5c0b758ea759ac914
alpinangu <inangualp@gmail.com>
2025-09-10 21:01:22 -0700

    const fix

--- nestkernel/simulation_manager.cpp
+++ nestkernel/simulation_manager.cpp
@@ -1135,7 +1135,7 @@ nest::SimulationManager::update_()
 
             const double 
 
-            const long end_local_spike_counter = kernel().event_delivery_manager.get_local_spike_counter();
+            long end_local_spike_counter = kernel().event_delivery_manager.get_local_spike_counter();
             const long local_spike_counter = end_local_spike_counter - start_local_spike_counter;
             start_local_spike_counter = end_local_spike_counter;
 
